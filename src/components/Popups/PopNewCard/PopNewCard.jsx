429b6f3a2e4712c9b2a34726b50e51f2da740795
didusanatoliy <bereg.007@mail.ru>
2024-05-16 23:24:17 +0300

    правки внес

--- src/components/Popups/PopNewCard/PopNewCard.jsx
+++ src/components/Popups/PopNewCard/PopNewCard.jsx
@@ -14,7 +14,7 @@ function PopNewCard() {
                 <div className="form-new__block">
                   <label htmlFor="formTitle" className="subttl">Название задачи</label>
                   <input className="form-new__input" type="text" name="name" id="formTitle"
-                         placeholder="Введите название задачи..." autofocus=""/>
+                         placeholder="Введите название задачи..." autoFocus=""/>
                 </div>
                 <div className="form-new__block">
                   <label htmlFor="textArea" className="subttl">Описание задачи</label>
